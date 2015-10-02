aced1f69d035b537655ffd14200c2259cc8ef45d
bergi <bergi@axolotlfarm.org>
2015-10-03 00:25:12 +0200

    adapted rdf-ext import without function call

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 /* global describe, it */
-
 var assert = require('assert')
-var rdf = require('rdf-ext')()
+var rdf = require('rdf-ext')
 var N3Serializer = require('../')
 
 var simpleGraph = rdf.createGraph()
